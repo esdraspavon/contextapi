5de8c0739de5e11d9537f83e9191104536bc1713
Esdras Pavon <pavonesdras@gmail.com>
2019-04-21 09:37:03 -0300

    ready to start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import Teams from "./components/Teams";
 class App extends Component {
+  state = {
+    equipos: [
+      {
+        nombre: "Real Madrid",
+        titulos: 35
+      },
+      {
+        nombre: "Barcelona",
+        titulos: 25
+      },
+      {
+        nombre: "Atlético de Madrid",
+        titulos: 10
+      }
+    ]
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Teams />
       </div>
     );
   }
---
+++ src/components/Teams.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Teams extends Component {
+  render() {
+    return <p>Equipos</p>;
+  }
+}
+
+export default Teams;
